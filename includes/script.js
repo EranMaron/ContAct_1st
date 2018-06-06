94acfbd97ef07b59a0369ecec01f86a0a6ba0e02
EranMaron <eran9maron@gmail.com>
2018-06-06 23:00:26 +0300

    CheckOut Page was added

--- includes/script.js
+++ includes/script.js
@@ -8,9 +8,19 @@ window.onload = function() {
     lightboxMessage.style.display = "none";
     var signBtn = document.getElementById("signBtn");
     signBtn.onclick = function() {lightbox(lightBox, lightboxMessage)};
+
+    
 }
 
 function lightbox(lightBox, lightBoxMessage) {
     lightBox.style.display = "block";
     lightBoxMessage.style.display = "block";
+}
+
+function closeLightbox() {
+    var lightBox = document.getElementById("lightboxBg");
+    var lightBoxMessage = this.document.getElementById("lightbox");
+    lightBox.style.display = "none";
+    lightBoxMessage.style.display = "none";
+    console.log("LightBox Closed");
 }
